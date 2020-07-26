b09018108a30b3f97fa2167c2edd81beea199efb
Gavince <1259781128@qq.com>
2020-07-27 07:26:53 +0800

    增加剑指offer

---
+++ 剑指offer/0.斐波那契数列.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/7/26 下午8:51
+# @Author  : gavin
+# @FileName: 0.斐波那契数列.py
+# @Software: PyCharm
+# @Blog    ：https://blog.csdn.net/weixin_35154281
+"""
+问题描述：
+１　１　２　３　５　８　... F(n) = F(n-1) + F(n-2)
+"""
+
+
+class Solution:
+
+    def fibonacci(self, n):
+        """递归算法(时间复杂度极高O(n)=２^n)"""
+        if n == 0:
+            return 0
+        if n == 1 or n == 2:
+            return 1
+        if n > 2:
+            return self.fibonacci(n - 1) + self.fibonacci(n - 2)
+
+    def fibonacci1(self, n):
+        """动态规划问题"""
+
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        if n > 1:
+            a = 1  # 较大值
+            b = 0  # 较小值
+            ret = 0
+            for i in range(2, n + 1):  # 左闭右开
+                ret = a + b
+                b = a
+                a = ret
+
+            return ret
+
+    def fibonacci2(self, n):
+        """列表写法O(n)=n"""
+
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        result = [0, 1]
+        for i in range(2, n + 1):
+            result.append(result[i - 1] + result[i - 2])
+
+        return result[-1]
+
+
+if __name__ == "__main__":
+    obj = Solution()
+    print("F1:", obj.fibonacci(10))
+    print("F2:", obj.fibonacci1(10))
+    print("F3:", obj.fibonacci2(10))
